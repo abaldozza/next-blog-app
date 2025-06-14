b3f4e88b160e3dbd39f9fa44e8b8dd1beae4e020
the-coder-zzz <21-06300@g.batstate-u.edu.ph>
2025-06-14 22:29:25 +0800

    updates

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -24,13 +24,13 @@ const Header = () => {
         <div className='py-5 px-5 md:px-12 lg:px-28'>
             <div className='flex justify-between items-center'>
                 <Image src={assets.logo} width={180} alt='' className="w-[130px] sm:w-auto" />
-                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow}></Image></button>
+                {/* <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow} alt=''></Image></button> */}
             </div>
             <div className='text-centered my-8'>
-                <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
-                <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos dolore atque necessitatibus consequuntur expedita sit nesciunt saepe, corporis ex minus a libero itaque autem assumenda unde, reprehenderit nulla quidem nihil?</p>
-                <form onSubmit={onSubmitHandler} className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder='Enter your email' className='pl-4 outline-none' />
+                <h1 className='text-3xl sm:text-5xl font-medium text-center'>Latest Blogs</h1>
+                <p className='mt-10 max-w-[740px] m-auto text-xs text-center sm:text-base'>Dive into our latest blogs for fresh perspectives and engaging stories. Discover new insights and stay informed with our most recent posts.</p>
+                <form onSubmit={onSubmitHandler} className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000] rounded-xl" action="">
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder='Enter your email' className='pl-4 outline-none rounded-xl' />
                     <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
                 </form>
             </div>
